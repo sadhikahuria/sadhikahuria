3d9070cc2e17a1cfe70dd97b7f743ffe17578da2
sadhikahuria <sadhikahuria@gmail.com>
2025-02-20 15:16:52 -0800

    Replace html functions by creating a js file. Styling the navigation bar so that it highlights the current page.

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleDarkMode(){
+    document.body.classList.toggle('dark-mode');
+    const mode = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+    localStorage.setItem('theme', mode);
+    updateButtonText();
+}
+
+function updateButtonText(){
+    const button = document.querySelector(".dark-mode-toogle");
+    if ( localStorage.getItem('theme') == 'dark') {
+        button.textContent = "Light Mode";
+
+    } else {
+        button.textContent = "Dark Mode";
+    }
+}
+
+function highlightCurrentPage(){
+    const currentPage = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPage){
+            link.classList.add("active-page");
+        } else {
+            link.classList.remove("active-page");
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+    updateButtonText();
+    highlightCurrentPage();
+});
